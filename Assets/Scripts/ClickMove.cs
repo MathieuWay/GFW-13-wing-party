359d1b95ba30710ec01a4da8860a692bb6eb47f0
Unknown <diyobe2207@gmail.com>
2019-09-03 10:02:32 +0200

    Prototype air hockey
    Scène ajoutée et scripts

---
+++ Assets/Scripts/ClickMove.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickMove : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+    private Vector3 mousePosition;
+    private Vector2 direction;
+    private bool followFinger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        followFinger = false;
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (followFinger)
+        {
+            MoveWithMouse();
+        }
+    }
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            followFinger = true;
+        }
+    }
+    void MoveWithMouse()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            direction = (mousePosition - transform.position).normalized;
+            rb.velocity = new Vector2(direction.x * speed, direction.y * speed);
+        }
+        else
+        {
+            followFinger = false;
+        }
+    }
+}
