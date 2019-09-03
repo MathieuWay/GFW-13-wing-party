bac407ff13ed78cf88125095fcaea3dd72525c76
Nagdalam <chariou.benjamin@gmail.com>
2019-09-03 16:42:23 +0200

    un peu plus de l'interface

--- Assets/Script/ScoreScript.cs
+++ Assets/Script/ScoreScript.cs
@@ -2,18 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ScoreScript : MonoBehaviour
 {
 
     public static int scoreValue_p1=0;
     public static int scoreValue_p2=0;
+    private static float timer;
+    private int wins_p1=0;
+    private int wins_p2=0;
     Text score;
+    [SerializeField] Text winnerName;
+    
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreValue_p1=0;
+        scoreValue_p2=0;
         score = GetComponent<Text>();
 
     }
@@ -21,6 +29,7 @@ public class ScoreScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (score.tag == "Player1" )
         {
             score.text = "Score : "+scoreValue_p1;
@@ -30,20 +39,47 @@ public class ScoreScript : MonoBehaviour
         {
             score.text = "Score : "+scoreValue_p2;
         }
+
+        if (scoreValue_p1>scoreValue_p2)
+        {
+            winnerName.text = "Le joueur 1 remporte cette manche";
+            
+        }
+
+        if (scoreValue_p1<scoreValue_p2)
+        {
+            winnerName.text = "Le joueur 2 remporte cette manche";
+
+        }
+
+        if (scoreValue_p1==scoreValue_p2)
+        {
+            winnerName.text = "Egalité ! Chaque joueur remporte un point";
+
+        }
+        timer=CountdownTimer.currentTime;
+
+        if(timer<=0){
+        Debug.Log("Le timer est passé dans le négatif");
+    }
         
     }
 
+    
     public void Scoretest_p1()
     {
 
-        scoreValue_p1 +=10;
+        scoreValue_p1 +=1;
 
     }
+
 
     public void Scoretest_p2()
     {
 
-        scoreValue_p2 +=10;
+        scoreValue_p2 +=1;
 
     }
+
+
 }
--- Assets/Script/TutorialSystem.cs
+++ Assets/Script/TutorialSystem.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class TutorialSystem : MonoBehaviour
 {
+    private int countdown=0;
+    private int skipcount_p1=0;
+    private int skipcount_p2=0;
+    public GameObject text1, text2, text3, text4, tutorialPopUp, HUD;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,61 @@ public class TutorialSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(skipcount_p1==skipcount_p2){
+
+            countdown=skipcount_p1;
+
+        }
         
+        if(countdown==1){
+            text1.SetActive(false);
+            text2.SetActive(true);
+        }
+        if(countdown==2){
+            text2.SetActive(false);
+            text3.SetActive(true);
+        }
+        if(countdown==3){
+            text3.SetActive(false);
+            text4.SetActive(true);
+        }
+        if(countdown==4){
+            tutorialPopUp.SetActive(false);
+            HUD.SetActive(true);
+        }
     }
+
+    public void Skip_p1(){
+        
+        if(countdown==0){
+            skipcount_p1=1;
+        }
+        if(countdown==1){
+            skipcount_p1=2;
+        }
+        if(countdown==2){
+            skipcount_p1=3;
+        }
+        if(countdown==3){
+            skipcount_p1=4;
+        }
+
+    }
+
+    public void Skip_p2(){
+        
+        if(countdown==0){
+            skipcount_p2=1;
+        }
+        if(countdown==1){
+            skipcount_p2=2;
+        }
+        if(countdown==2){
+            skipcount_p2=3;
+        }
+        if(countdown==3){
+            skipcount_p2=4;
+        }
+}
+
 }
