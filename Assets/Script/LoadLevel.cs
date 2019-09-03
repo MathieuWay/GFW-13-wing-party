a3c8103e362b87856e4c8e7f4c9cdf24926d0c2a
Nagdalam <chariou.benjamin@gmail.com>
2019-09-03 10:05:40 +0200

    benjamin modif

---
+++ Assets/Script/LoadLevel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadLevel : MonoBehaviour{
+
+
+public void LoadLvl(string nameScene)
+{
+    UnityEngine.SceneManagement.SceneManager.LoadScene(nameScene);
+}
+
+}
